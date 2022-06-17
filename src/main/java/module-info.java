6ec07c3cfb369bebd4272f9b9c869707767ce25b
Gustavo Ribeiro <99373133+gustavosett@users.noreply.github.com>
2022-06-16 23:23:20 -0300

    bug fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,10 @@ module novo.crud {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.sql;
 
     opens novo.crud to javafx.fxml;
     exports novo.crud;
+
+    
 }
--- target/classes/novo/crud/MainController.java
+++ target/classes/novo/crud/MainController.java
@@ -5,10 +5,18 @@
 package novo.crud;
 
 import java.net.URL;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
@@ -38,7 +46,7 @@ public class MainController implements Initializable {
     @FXML
     private TableColumn<Pessoa, Integer> colIdade;
     @FXML
-    private TableColumn<Pessoa, String> colSexo;
+    private TableColumn<Pessoa, Character> colSexo;
     @FXML
     private Button btnCadastrar;
     @FXML
@@ -59,5 +67,44 @@ public class MainController implements Initializable {
     private void handleButtonAction(ActionEvent event) {
     }
     
+    public Connection getConnection(){
+        Connection conn;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/usuarios", "root", "");
+            return conn;
+        }catch (Exception ex){
+            System.out.println("Erro: " + ex.getMessage());
+            return null;
+        }
+    }
 
+    public ObservableList<Pessoa> getListaUsuarios(){
+        ObservableList<Pessoa> listaPessoa = FXCollections.observableArrayList();
+        Connection conn = getConnection();
+        String query = "SELECT * FROM usuarios";
+        Statement st;
+        ResultSet rs;
+
+        try {
+            st = conn.createStatement();
+            rs = st.executeQuery(query);
+            Pessoa pessoa;
+            while(rs.next()){
+                pessoa = new Pessoa(rs.getInt("id"), rs.getString("nome"), rs.getInt("idade"), rs.getString("sexo").charAt(0));
+                listaPessoa.add(pessoa);
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return listaPessoa;
+    }
+
+    public void mostraUsuarios(){
+        ObservableList<Pessoa> list = getListaUsuarios();
+
+        colID.setCellValueFactory(new PropertyValueFactory<Pessoa, Integer>("id"));
+        colNome.setCellValueFactory(new PropertyValueFactory<Pessoa, String>("nome"));
+        colIdade.setCellValueFactory(new PropertyValueFactory<Pessoa, Integer>("idade"));
+        colSexo.setCellValueFactory(new PropertyValueFactory<Pessoa, Character>("sexo"));
+    }
 }
